6f08aa633646a8378e6a97423f06b3f10ca6bbe8
bharath0616 <108422500+bharath0616@users.noreply.github.com>
2024-04-20 02:33:57 +0530

    requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='EcoVision',
+    version='0.0.0',
+    author='Bharath',
+    author_email='mbharath1603@gmail.com',
+    packages=find_packages(),
+    install_requires= []
+)
